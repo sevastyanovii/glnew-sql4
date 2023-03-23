0aa2637695c1c6db19ac1d70d143f3530ff5fd80
Sergey Vasilyev <kasiavc@gmail.com>
2023-03-23 10:48:47 +0300

    Новая версия FSD 0.06 по сервису. Доработка справочника currency

--- SQL/TABLES/CURRENCY.sql
+++ SQL/TABLES/CURRENCY.sql
@@ -3,13 +3,19 @@ CREATE TABLE currency
     glccy  CHAR(3) PRIMARY KEY,
     cbccy  CHAR(3) NOT NULL,
     cynm   VARCHAR2(35 CHAR) NOT NULL,
-    lcd    DATE,
-    nbdp   NUMBER(10,0) DEFAULT 0 NOT NULL
+    nbdp   NUMBER(10,0) DEFAULT 0 NOT NULL,
+    cbrate_set CHAR(1),
+    dto    DATE,
+    dtc    DATE,
+    act    CHAR(1)
 );
 
 COMMENT ON TABLE currency IS 'Справочник валют';
 COMMENT ON COLUMN currency.glccy  IS 'Символьный код валюты';
 COMMENT ON COLUMN currency.cbccy  IS 'Цифровой код валюты для номера счета';
 COMMENT ON COLUMN currency.cynm   IS 'Сокращенное название валюты';
-COMMENT ON COLUMN currency.lcd    IS 'Дата регистрации кода валюты';
 COMMENT ON COLUMN currency.nbdp   IS 'Количество «копеек» в единице валюты (десятичный логарифм числа минорных единиц в номинальной)';
+COMMENT ON COLUMN currency.cbrate_set   IS 'Установлен курс ЦБ. 1 – Да, 0 - Нет';
+COMMENT ON COLUMN currency.dto   IS 'Дата активации кода валюты';
+COMMENT ON COLUMN currency.dtc   IS 'Дата деактивации кода валюты';
+COMMENT ON COLUMN currency.act   IS 'Признак активности валюты A – активная, Z - неактивная';
